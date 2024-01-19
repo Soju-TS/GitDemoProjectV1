c15c2963f7fe13619eb33827e0458c3131a8fe85
Soju-TS <sojats20001@gmail.com>
2024-01-20 00:12:13 +0530

    Firstcommit

---
+++ src/Test1.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		System.out.println("Number defined is"+ " "+num1);
+		boolean flag = true;
+		System.out.println("value defined is"+" "+flag);
+		
+	}
+
+}
