8752dc393d606810af42897ddbb6ff6d3ec91124
fabee <fabee@fabeenabineesh>
2024-01-23 23:34:13 +0400

    Commiting test2 to BranchX

---
+++ src/Test2.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 5;
+		
+		System.out.println("Number defined is"+ " "+num1);
+		boolean flag = true;
+		System.out.println("value defined is"+" "+flag);
+		System.out.println("hai");
+		
+	}
+
+}
